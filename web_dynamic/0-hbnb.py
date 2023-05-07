9251a06f030d906f1a62fdab07123951fe8f019f
Amarajah <naomi.ibeh69@gmail.com>
2023-05-07 14:41:49 -0700

    updated some files

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -4,7 +4,12 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
-import uuid;
+from models.state import State
+from models.city import City
+from models.amenities import Amenities
+from models.place import Place
+from os import environs
+import uuid
 
 # flask setup
 app = Flask(__name__)
--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -4,7 +4,12 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
-import uuid;
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+import uuid
 
 # flask setup
 app = Flask(__name__)
